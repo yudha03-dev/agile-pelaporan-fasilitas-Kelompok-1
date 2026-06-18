270f7613d4bdb018117efa29056d03bad5d66cf4
virly yudha <yudharinanda789@gmail.com>
2026-06-18 14:22:46 +0700

    update database campusfix

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
 const app = express();
+const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("CampusFix Berjalan");
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
-app.listen(3000, () => {
-  console.log("Server berjalan di port 3000");
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
